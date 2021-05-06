3d19085ff74ece0dcfc5bd305fdaa4bd7772c142
Igor-Ferraz7 <igorsousaferrazaraga2@gmail.com>
2021-05-05 21:50:33 -0300

    Criado a classe Pessoa

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,2 +1,8 @@
 class Pessoa:
-    pass
+    def cumprimentar(self):
+        return f'Olá, id de self: {id(self)}'
+
+
+if __name__ == "__main__":
+    p = Pessoa()
+    print(f"{p.cumprimentar()} | id de p:{id(p)}")
