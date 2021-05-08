13347eec89ea6da2f0b1cd90760f0dbf8dbddf29
Igor-Ferraz7 <igorsousaferrazaraga2@gmail.com>
2021-05-07 23:30:17 -0300

    Criado atributo complexo: filhos.

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,17 +1,17 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=16):
+    def __init__(self, *filhos, nome=None, idade=16):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
     def cumprimentar(self):
         return f'Olá, id de self: {id(self)}'
 
 
 if __name__ == "__main__":
-    p = Pessoa('Rogi', 61)
-    print(f"{p.cumprimentar()} | id de p:{id(p)}")
-    print(p.nome)
-    print(p.idade)
-    p.nome = "Igor"
-    p.idade = 16
-    print(p.nome)
-    print(p.idade)
+    Renzo = Pessoa(nome='Renzo')
+    Igor = Pessoa(Renzo, nome='Igor')
+    print(f"{Igor.cumprimentar()} | id de p:{id(Igor)}")
+    print(Igor.nome)
+    print(Igor.idade)
+    for filho in Igor.filhos:
+        print(filho.nome)
